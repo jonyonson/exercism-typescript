97c6c741767fe42c858216f95804735554bb1ff3
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-08 15:00:36 -0500

    wrap value in function

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,20 @@
-function isLeapYear(year: number): boolean {
-  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+function makeNumeric(n: number) {
+  return {
+    isDivisibleBy: (divisor: number): boolean => n % divisor === 0,
+  };
+}
+
+function isLeapYear(n: number): boolean {
+  let year = makeNumeric(n);
+
+  return (
+    year.isDivisibleBy(4) &&
+    (!year.isDivisibleBy(100) || year.isDivisibleBy(400))
+  );
 }
 
 export default isLeapYear;
+
+// function isLeapYear(year: number): boolean {
+//   return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+// }
