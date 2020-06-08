61d681da105783c494fa2214c72f86818fad6733
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-08 13:30:04 -0500

    refactor

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,12 +1,5 @@
 function isLeapYear(year: number): boolean {
-  let leap = false;
-  if (year % 4 === 0) {
-    leap = true;
-    if (year % 100 == 0) leap = false;
-    if (year % 400 == 0) leap = true;
-  }
-
-  return leap;
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 export default isLeapYear;
