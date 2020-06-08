2bb113470aa6781a6e5b49b82ba264cd1b3f2425
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-08 11:21:03 -0500

    refactor

--- rotational-cipher/rotational-cipher.ts
+++ rotational-cipher/rotational-cipher.ts
@@ -1,29 +1,40 @@
-function encrypt(char: string, rotations: number): string {
-  const code = char.charCodeAt(0);
-  let newCode = code + rotations;
+export default class RotationalCipher {
+  private static readonly UPPERCASE_A_CODE = 'A'.charCodeAt(0);
+  private static readonly UPPERCASE_Z_CODE = 'Z'.charCodeAt(0);
+  private static readonly LOWERCASE_A_CODE = 'a'.charCodeAt(0);
+  private static readonly LOWERCASE_Z_CODE = 'z'.charCodeAt(0);
 
-  const CAPITAL_A = 'A'.charCodeAt(0);
-  const CAPITAL_Z = 'Z'.charCodeAt(0);
-  const LOWER_A = 'a'.charCodeAt(0);
-  const LOWER_Z = 'z'.charCodeAt(0);
-
-  if (code >= CAPITAL_A && code <= CAPITAL_Z) {
-    if (newCode > CAPITAL_Z) {
-      newCode = CAPITAL_A - 1 + newCode - CAPITAL_Z;
-    }
-  } else if (code >= LOWER_A && code <= LOWER_Z) {
-    if (newCode > LOWER_Z) {
-      newCode = LOWER_A - 1 + newCode - LOWER_Z;
-    }
-  } else {
-    return char;
+  static rotate(text: string, rotations: number): string {
+    return [...text]
+      .map((char) => RotationalCipher.encrypt(char, rotations))
+      .join('');
   }
 
-  return String.fromCharCode(newCode);
-}
+  private static encrypt(char: string, rotations: number): string {
+    if (char.match(/[a-z]/i)) {
+      const code = char.charCodeAt(0);
 
-export default class RotationalCipher {
-  static rotate(text: string, rotations: number): string {
-    return [...text].map((char) => encrypt(char, rotations)).join('');
+      // Uppercase letters
+      if (
+        code >= RotationalCipher.UPPERCASE_A_CODE &&
+        code <= RotationalCipher.UPPERCASE_Z_CODE
+      ) {
+        return String.fromCharCode(
+          ((code - RotationalCipher.UPPERCASE_A_CODE + rotations) % 26) +
+            RotationalCipher.UPPERCASE_A_CODE,
+        );
+        // Lowercase letters
+      } else if (
+        code >= RotationalCipher.LOWERCASE_A_CODE &&
+        code <= RotationalCipher.LOWERCASE_Z_CODE
+      ) {
+        return String.fromCharCode(
+          ((code - RotationalCipher.LOWERCASE_A_CODE + rotations) % 26) +
+            RotationalCipher.LOWERCASE_A_CODE,
+        );
+      }
+    }
+
+    return char;
   }
 }
