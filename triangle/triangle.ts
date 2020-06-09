246a29179b7f8614b8933fb61c957fddf8b3b02e
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-08 22:52:22 -0500

    refactor

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -7,27 +7,29 @@ export default class Triangle {
     this.sides = sides;
   }
 
-  kind(): TriangleType {
-    const sortedSides = this.sides.sort((a, b) => a - b);
+  private checkForValidTriangle(): void {
+    const [side1, side2, side3] = this.sides.sort((a, b) => a - b);
     // Throw if any sides are less than or equal to 0
-    if (sortedSides[0] <= 0) {
+    if (side1 <= 0) {
       throw new Error('The length of all sides have to be greater than 0');
     }
     // Throw if the sum of any two sides are less than the third
-    if (sortedSides[0] + sortedSides[1] < sortedSides[2]) {
+    if (side1 + side2 < side3) {
       throw new Error(
         'The sum of the lengths of any two sides must be greater than or equal to the length of the third side.',
       );
     }
+  }
+
+  kind(): TriangleType {
+    this.checkForValidTriangle();
 
     let triangle: TriangleType;
+    const [side1, side2, side3] = this.sides;
 
-    if (this.sides.every((side) => side === this.sides[0])) {
+    if (side1 === side2 && side2 === side3) {
       triangle = 'equilateral';
-    } else if (
-      sortedSides[0] == sortedSides[1] ||
-      sortedSides[1] == sortedSides[2]
-    ) {
+    } else if (side1 === side2 || side1 === side3 || side2 === side3) {
       triangle = 'isosceles';
     } else {
       triangle = 'scalene';
