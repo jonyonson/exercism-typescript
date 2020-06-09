47f24eb03defd80085d67d62abac6a05e69dcf6c
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-09 08:04:31 -0500

    space-age

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -7,21 +7,40 @@ export default class SpaceAge {
     this.secondsInYear = 31557600;
   }
 
-  onEarth(): number {}
+  onEarth(): number {
+    return this.formatYears(1);
+  }
 
-  onMercury(): number {}
+  onMercury(): number {
+    return this.formatYears(0.2408467);
+  }
 
-  onVenus(): number {}
+  onVenus(): number {
+    return this.formatYears(0.61519726);
+  }
 
-  onMars(): number {}
+  onMars(): number {
+    return this.formatYears(1.8808158);
+  }
 
-  onJupiter(): number {}
+  onJupiter(): number {
+    return this.formatYears(11.862615);
+  }
 
-  onSaturn(): number {}
+  onSaturn(): number {
+    return this.formatYears(29.447498);
+  }
 
-  onUranus(): number {}
+  onUranus(): number {
+    return this.formatYears(84.016846);
+  }
 
-  onNeptune(): number {}
+  onNeptune(): number {
+    return this.formatYears(164.79132);
+  }
 
-  private formatYears(): number {}
+  private formatYears(orbitsPerEartYear: number): number {
+    const years = this.seconds / (this.secondsInYear * orbitsPerEartYear);
+    return +years.toFixed(2);
+  }
 }
