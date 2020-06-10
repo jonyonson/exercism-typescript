6c401f352bec70384850c20d00b0401323486114
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-10 11:36:02 -0500

    refactor

--- protein-translation/protein-translation.ts
+++ protein-translation/protein-translation.ts
@@ -7,7 +7,7 @@ type Protein =
   | 'Cysteine'
   | 'Tryptophan';
 
-const codonToProtein: { [key: string]: Protein | 'STOP' } = {
+const codonToProtein: { [key: string]: Protein } = {
   AUG: 'Methionine',
   UUU: 'Phenylalanine',
   UUC: 'Phenylalanine',
@@ -22,27 +22,33 @@ const codonToProtein: { [key: string]: Protein | 'STOP' } = {
   UGU: 'Cysteine',
   UGC: 'Cysteine',
   UGG: 'Tryptophan',
-  UAA: 'STOP',
-  UAG: 'STOP',
-  UGA: 'STOP',
 };
+
+const stopCodons = ['UAA', 'UAG', 'UGA'];
 
 class ProteinTranslation {
   static proteins(rnaSequence: string): Protein[] {
+    const codons = this.codons(rnaSequence);
     const proteins: Protein[] = [];
-    for (let i = 0; i < rnaSequence.length; i += 3) {
-      const codon = rnaSequence
-        .split('')
-        .slice(i, i + 3)
-        .join('');
 
-      const protein = codonToProtein[codon];
-      if (protein === 'STOP') break;
-
+    for (let i = 0; i < codons.length; i++) {
+      if (stopCodons.includes(codons[i])) break;
+      const protein = codonToProtein[codons[i]];
       proteins.push(protein);
     }
 
     return proteins;
+  }
+
+  private static codons(rna: string): string[] {
+    const codons: string[] = [];
+
+    for (let i = 0; i < rna.length; i += 3) {
+      const codon = [...rna].slice(i, i + 3).join('');
+      codons.push(codon);
+    }
+
+    return codons;
   }
 }
 
