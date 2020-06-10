dd8093869371f7d2a5745b1226dbff77499acd22
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-10 11:55:05 -0500

    add check for valid dna

--- nucleotide-count/nucleotide-count.ts
+++ nucleotide-count/nucleotide-count.ts
@@ -2,12 +2,26 @@ type Dna = 'A' | 'C' | 'G' | 'T';
 
 class NucleotideCount {
   static nucleotideCounts(dna: string) {
-    const nucleotideCount = { A: 0, C: 0, G: 0, T: 0 };
+    this.isValidDNA(dna);
+
+    const nucleotideCount: { [key in Dna]: number } = {
+      A: 0,
+      C: 0,
+      G: 0,
+      T: 0,
+    };
+
     for (let i = 0; i < dna.length; i++) {
       nucleotideCount[dna[i] as Dna]++;
     }
 
     return nucleotideCount;
+  }
+
+  private static isValidDNA(dna: string) {
+    if (!/^$|^[ACGT]+$/.test(dna)) {
+      throw new Error('Invalid nucleotide in strand');
+    }
   }
 }
 
