4c8dc40fcd99e928ac28babf0b6c33520cd49080
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-10 00:03:45 -0500

    protein translation

--- protein-translation/protein-translation.ts
+++ protein-translation/protein-translation.ts
@@ -1,7 +1,49 @@
+type Protein =
+  | 'Methionine'
+  | 'Phenylalanine'
+  | 'Serine'
+  | 'Leucine'
+  | 'Tyrosine'
+  | 'Cysteine'
+  | 'Tryptophan';
+
+const codonToProtein: { [key: string]: Protein | 'STOP' } = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
+  UCC: 'Serine',
+  UCA: 'Serine',
+  UCG: 'Serine',
+  UAU: 'Tyrosine',
+  UAC: 'Tyrosine',
+  UGU: 'Cysteine',
+  UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: 'STOP',
+  UAG: 'STOP',
+  UGA: 'STOP',
+};
+
 class ProteinTranslation {
-    static proteins( /* Parameters go here */ ) {
-        // Your code here
+  static proteins(rnaSequence: string): Protein[] {
+    const proteins: Protein[] = [];
+    for (let i = 0; i < rnaSequence.length; i += 3) {
+      const codon = rnaSequence
+        .split('')
+        .slice(i, i + 3)
+        .join('');
+
+      const protein = codonToProtein[codon];
+      if (protein === 'STOP') break;
+
+      proteins.push(protein);
     }
+
+    return proteins;
+  }
 }
 
-export default ProteinTranslation
+export default ProteinTranslation;
