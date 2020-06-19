681449edd236537b7b913eda6c4aa0aadad51619
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-19 10:02:20 -0500

    robot-name: first pass

---
+++ robot-name/robot-name.ts
@@ -0,0 +1,51 @@
+class RobotName {
+  name: string;
+  private usedNames: Set<string>;
+
+  constructor() {
+    this.usedNames = new Set();
+    this.name = this.generateRandomName();
+  }
+
+  resetName() {
+    this.name = this.generateRandomName();
+  }
+
+  private generateRandomName(): string {
+    const randomName = this.generatePrefix() + this.generateSuffix();
+    if (this.usedNames.has(randomName)) {
+      return this.generateRandomName();
+    } else {
+      this.usedNames.add(randomName);
+    }
+
+    return randomName;
+  }
+
+  private generatePrefix(): string {
+    return this.generateUppercaseLetter() + this.generateUppercaseLetter();
+  }
+
+  private generateSuffix(): string {
+    return (
+      this.generateRandomDigit() +
+      this.generateRandomDigit() +
+      this.generateRandomDigit()
+    );
+  }
+
+  private generateUppercaseLetter(): string {
+    const A_CHARCODE = 65;
+    const Z_CHARCODE = 90;
+    const charCode =
+      Math.floor(Math.random() * (Z_CHARCODE - A_CHARCODE + 1)) + A_CHARCODE;
+
+    return String.fromCharCode(charCode);
+  }
+
+  private generateRandomDigit(): string {
+    return Math.floor(Math.random() * 10).toString();
+  }
+}
+
+export default RobotName;
