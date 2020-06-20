177c6db7dd040154d02cf3cb939c70bebdfeac4e
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-19 23:14:24 -0500

    hamming: first pass

---
+++ hamming/hamming.ts
@@ -0,0 +1,20 @@
+class Hamming {
+  compute(strandA: string, strandB: string) {
+    this.assertValidLengths(strandA, strandB);
+
+    let distance = 0;
+    for (let i = 0; i < strandA.length; i++) {
+      if (strandA[i] !== strandB[i]) distance++;
+    }
+
+    return distance;
+  }
+
+  private assertValidLengths(a: string, b: string) {
+    if (a.length !== b.length) {
+      throw new Error('DNA strands must be of equal length.');
+    }
+  }
+}
+
+export default Hamming;
