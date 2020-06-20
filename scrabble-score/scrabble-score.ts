1a9c250f92a4cea7b61adbf17577fbc10e45666d
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-20 17:16:48 -0500

    scrabble-score: first pass

---
+++ scrabble-score/scrabble-score.ts
@@ -0,0 +1,37 @@
+const tileValues: { [key: string]: number } = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
+export default function score(word: string | undefined): number {
+  if (word == null) return 0;
+
+  let score = 0;
+  [...word].forEach((letter) => (score += tileValues[letter.toUpperCase()]));
+
+  return score;
+}
