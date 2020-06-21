5783a3347d7b615500ea4bcaec5b7712dac9f9f9
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-20 19:44:28 -0500

    perfect-numbers: first pass

---
+++ perfect-numbers/perfect-numbers.ts
@@ -0,0 +1,33 @@
+type Classification = 'perfect' | 'abundant' | 'deficient';
+
+export default class PerfectNumbers {
+  static classify(number: number): Classification {
+    this.assertNaturalNumber(number);
+
+    const factors = this.factors(number);
+    const aliquotSum = this.aliquotSum(factors);
+
+    if (aliquotSum > number) {
+      return 'abundant';
+    } else if (aliquotSum < number) {
+      return 'deficient';
+    } else {
+      return 'perfect';
+    }
+  }
+
+  private static factors(n: number): number[] {
+    const oneToN = Array.from({ length: n }, (_, i) => i + 1);
+    return oneToN.filter((i) => n % i === 0 && i !== n);
+  }
+
+  private static aliquotSum(numbers: number[]): number {
+    return [...numbers].reduce((acc, cv) => acc + cv, 0);
+  }
+
+  private static assertNaturalNumber(number: number): void {
+    if (number < 1) {
+      throw new Error('Classification is only possible for natural numbers.');
+    }
+  }
+}
