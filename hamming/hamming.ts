5634a239ec01e29a538f7697fae5437efe844fe3
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-28 23:19:09 -0500

    hamming: replace for loop with for...in

--- hamming/hamming.ts
+++ hamming/hamming.ts
@@ -1,16 +1,18 @@
 class Hamming {
-  compute(strandA: string, strandB: string) {
+  compute(strandA: string, strandB: string): number {
     this.assertValidLengths(strandA, strandB);
 
     let distance = 0;
-    for (let i = 0; i < strandA.length; i++) {
-      if (strandA[i] !== strandB[i]) distance++;
+    for (const i in [...strandA]) {
+      if (strandA[i] !== strandB[i]) {
+        distance++;
+      }
     }
 
     return distance;
   }
 
-  private assertValidLengths(a: string, b: string) {
+  private assertValidLengths(a: string, b: string): void {
     if (a.length !== b.length) {
       throw new Error('DNA strands must be of equal length.');
     }
