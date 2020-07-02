05a16c9298d48cd1de1ff729ac7190611368dd7f
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-07-02 10:58:40 -0500

    luhn: deal with numbers not string

--- luhn/luhn.ts
+++ luhn/luhn.ts
@@ -1,38 +1,41 @@
 class Luhn {
-  static valid(digitSequence: string): boolean {
-    if (this.isInvalidInput(digitSequence)) return false;
-    const everyOtherDoubled = this.doubleEveryOtherDigit(digitSequence);
+  static valid(sequence: string): boolean {
+    if (this.isInvalidInput(sequence)) return false;
+    const digits = this.digits(sequence);
+    const everyOtherDoubled = this.doubleEveryOtherDigit(digits);
     const sum = this.sum(everyOtherDoubled);
 
     return this.isDivisibleBy10(sum);
   }
 
-  static doubleEveryOtherDigit(sequence: string): string {
-    sequence = sequence.replace(/\s/g, '');
-    return [...sequence]
-      .map((char, i) => {
-        if ((sequence.length - i) % 2 === 0) {
-          let doubled = Number(char) * 2;
-          if (doubled > 9) doubled -= 9;
-
-          return doubled;
-        }
-
-        return char;
-      })
-      .join('');
+  static doubleEveryOtherDigit(digits: number[]): number[] {
+    // sequence = sequence.replace(/\s/g, '');
+    // return [...sequence]
+    return digits.map((d, i) => {
+      if ((digits.length - i) % 2 === 0) {
+        let doubled = d * 2;
+        if (doubled > 9) doubled -= 9;
+        return doubled;
+      }
+      return d;
+    });
   }
 
   static isDivisibleBy10(sum: number): boolean {
     return sum % 10 == 0;
   }
 
-  static sum(digits: string): number {
-    return [...digits].reduce((acc, cv) => acc + Number(cv), 0);
+  static sum(digits: number[]): number {
+    return [...digits].reduce((acc, cv) => acc + cv, 0);
   }
 
   static isInvalidInput(input: string): boolean {
-    return input.length <= 1 || !/^[0-9 ]+$/.test(input);
+    return input.trim().length <= 1 || !/^[0-9 ]+$/.test(input);
+  }
+
+  static digits(sequence: string): number[] {
+    sequence = sequence.replace(/\s/g, '');
+    return [...sequence].map((c) => Number(c));
   }
 }
 
